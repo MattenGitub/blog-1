78cad8f51fb7b1b788d1d02dc6bf48d570fe9ec1
Emmanuelk2001 <121894373+Emmanuelk2001@users.noreply.github.com>
2023-01-28 22:26:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>my.blog</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1><span>My</span><span>.</span>blog</h1>
+            <div class="right">
+                <a href="#">
+                    <p>Blog</p>
+                </a>
+                <a href="#">
+                    <p>Contact</p>
+                </a>
+                <a href="#">
+                    <p>About us</p>
+                </a>
+                <a href="src/Connexion-Inscription/index.html"><button> Sign in </button></a>
+                <!-- <a href="src/Connexion-Inscription/index2.html"><button> Sign up</button></a> -->
+
+            </div>
+
+        </nav>
+        <section class="presentation">
+            <div class="abstract-presentation">
+                <h1>Build your blog in<br> few minutes</h1>
+                <p>Join the community shay your stories on your blog.</p>
+            </div>
+        </section>
+    </header>
+    <footer>
+        <div class="container">
+            <h3>Contactez-nous</h3>
+
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+
+        </div>
+    </footer>
+</body>
+
+</html>
